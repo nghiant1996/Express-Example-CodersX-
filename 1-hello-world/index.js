78687b2fb1473bb7d7a2e5ebf24c2da2fc07a212
NghiaNT <nghiant.hust@gmail.com>
2020-04-29 12:31:27 +0700

    Bai 3

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -3,6 +3,11 @@ var app = express();
 
 var port = 3000;
 
+var users = [
+ 	{id: 1, name: 'Nghia'},
+	{id: 2, name: 'Cuong'},
+];
+
 app.set('view engine', 'pug');
 app.set('views','./views');
 
@@ -11,16 +16,25 @@ app.get('/', function(request, response) {
 	response.render('index', {
 		name: 'Nghia NT'
 	});
+}); 
+
+app.get('/users/search', function(req,res) {
+	var q = req.query.q;
+	var matchedUsers = users.filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+
+	res.render('users/index', {
+		users: matchedUsers
+	})
 });
 
 app.get('/users', function (req, res) {
+
 	res.render('users/index', {
-		users: [
-			{id: 1, name: 'Nghia'},
-			{id: 2, name: 'Cuong'},
-		]
+		users: users
 	})
-})
+});
 
 
 app.listen(port, function() {
