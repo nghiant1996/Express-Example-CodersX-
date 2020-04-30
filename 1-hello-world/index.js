a8d829ac36f84021fb3e3bbc41d40f453e9790c7
NghiaNT <nghiant.hust@gmail.com>
2020-04-30 12:38:56 +0700

    post method

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var port = 3000;
 
@@ -10,6 +11,9 @@ var users = [
 
 app.set('view engine', 'pug');
 app.set('views','./views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 app.get('/', function(request, response) {
@@ -36,10 +40,21 @@ app.get('/users', function (req, res) {
 	})
 });
 
+app.get('/users/create', function(req,res) {
+	res.render('users/create')
+});
+
+app.post('/users/create', function(req,res) {
+	users.push(req.body);
+	res.redirect('/users');
+})
 
 app.listen(port, function() {
 	console.log('Serve listening on port ' + port);
 });
+
+
+
 
 
 
