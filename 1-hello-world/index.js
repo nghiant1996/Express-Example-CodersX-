853389d1254e9ae242ec18b5eba5eccf516d11b3
NghiaNT <nghiant.hust@gmail.com>
2020-05-02 09:59:33 +0700

    Bai 7-CodersX

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');//lowbd co nhieu adapte
 var adapter = new FileSync('db.json');
+var shortid = require('shortid');
+
+
 
 db = low(adapter);
 
@@ -28,6 +31,7 @@ app.get('/', function(request, response) {
 
 app.get('/users/search', function(req,res) {
 	var q = req.query.q;
+	var users = db.get('users').value();
 	var matchedUsers = users.filter(function(user) {
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
 	});
@@ -49,7 +53,18 @@ app.get('/users/create', function(req,res) {
 	res.render('users/create')
 });
 
+app.get('/users/:id', function(req, res) {
+	var id = req.params.id ;
+	var user = db.get('users').find({id: id}).value();
+
+	res.render('users/view', {
+		user: user
+	});
+})
+
 app.post('/users/create', function(req,res) {
+	req.body.id = shortid.generate();
+
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 	
