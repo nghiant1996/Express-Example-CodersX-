9b879c7362041e6da245633ddfb3476265b8bae3
Glitch (16-b1-md5) <none>
2020-05-24 04:40:22 +0000

    🎨🐽 Updated with Glitch

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,6 @@
 var db = require('../db.js');
 var md5 = require('md5');
 
-var bcrypt = require('bcrypt');
-var saltRounds = 10;
-
 module.exports.login = function(req,res){
   res.render('auth/login');
 }
@@ -24,21 +21,18 @@ module.exports.postLogin = function(req,res){
     return
   }
   
-  // var hashedPassword = md5(password);
+  var hashedPassword = md5(password);
   
-  var check = bcrypt.compareSync(password, user.password)      
-  
-  if(check === false){
-        res.render('auth/login',{
-        errors: [
-          'Wrong password'
-        ],
-        values: req.body
-        });
-        return
+  if(user.password != hashedPassword){
+    res.render('auth/login',{
+      errors: [
+        'Wrong password'
+      ],
+      values: req.body
+    });
+    return
   }
   
-    
   res.cookie('userId', user.id);
   res.redirect('/users');
   
